d9436f71599b940e031b2c0f8cc14ec8f2e19c70
Ana Beatriz <itzzzs2pudim@gmail.com>
2024-08-25 17:58:41 -0300

    addddd

--- Shadows_Of_Eternity/Assets/Scripts/PlayerMovement.cs
+++ Shadows_Of_Eternity/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 5f;
+    public float runSpeed = 10f;
+    private float currentSpeed;
     Vector2 movement;
 
     public Animator animator;
 
+    public Image staminaBar;
+    public float stamina = 100f, maxStamina = 100f;
+    public float runCost = 10f;
+    public bool isRunning = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpeed = speed;
+        stamina = maxStamina;
     }
 
     // Update is called once per frame
@@ -25,6 +34,22 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+            isRunning = true;
+            currentSpeed = runSpeed;
+            stamina -= runCost * Time.deltaTime;
+        }
+        else
+        {
+            isRunning = false;
+            currentSpeed = speed;
+            stamina += runCost * Time.deltaTime;
+        }
+
+        stamina = Mathf.Clamp(stamina, 0, maxStamina);
+        staminaBar.fillAmount = stamina / maxStamina;
     }
 
     void FixedUpdate()
